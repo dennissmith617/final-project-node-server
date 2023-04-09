12ae9a80123d29622194502655f5029e6e9dd220
jazlynnfok <54912902+jazlynnfok@users.noreply.github.com>
2023-04-09 16:08:07 -0400

    updated default img path

--- users/users-schema.js
+++ users/users-schema.js
@@ -9,7 +9,7 @@ const usersSchema = new mongoose.Schema(
         age: Number,
         booksRead: { type: Number, default: 0 },
         role: { type: String, default: "VIEWER", enum: ["VIEWER", "ADMIN", "CRITIC", "AUTHOR"] },
-        profilePicture: {type: String, default: "../../images/defaultProPic.jpeg"},
+        profilePicture: {type: String, default: "/images/defaultProPic.jpeg"},
         followers: [{type: mongoose.Types.ObjectId}],
         following: [{type: mongoose.Types.ObjectId}],
         comments: [{type: String}],
